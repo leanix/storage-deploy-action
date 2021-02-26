8e03b7b8699a2aca87b54efdcabd8e216ad2c13c
Yannick Epstein <yannick.epstein@leanix.net>
2021-02-26 11:10:05 +0100

    INVT-9643 fix rollback test workflow

--- .github/workflows/introduce_breaking_change.py
+++ .github/workflows/introduce_breaking_change.py
@@ -1,5 +1,7 @@
-with open("../../test/index.html", "w") as file:
+import pathlib
+
+with open(pathlib.Path('/test/index.html'), "w") as file:
     file.write("<h1>Breaking Change</h1>")
 
-with open("../../test/main.js", "w") as file:
+with open(pathlib.Path('/test/main.js'), "w") as file:
     file.write("console.log('Breaking Change')")
