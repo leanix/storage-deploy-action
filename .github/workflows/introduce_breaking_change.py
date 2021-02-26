67ebf42e61fd73b49de8b4b4bf814ed7b27c5dc1
Yannick Epstein <yannick.epstein@leanix.net>
2021-02-26 11:18:21 +0100

    INVT-9463 fix file paths

--- .github/workflows/introduce_breaking_change.py
+++ .github/workflows/introduce_breaking_change.py
@@ -1,9 +1,7 @@
 import pathlib
 
-print(pathlib.Path.cwd())
-
-with open(pathlib.Path('/test/index.html'), "w") as file:
+with open(pathlib.Path('./test/index.html'), "w") as file:
     file.write("<h1>Breaking Change</h1>")
 
-with open(pathlib.Path('/test/main.js'), "w") as file:
+with open(pathlib.Path('./test/main.js'), "w") as file:
     file.write("console.log('Breaking Change')")
