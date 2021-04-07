1f1045e041c04d28d5eb20aa65a83cdf7efee6fe
Yannick Epstein <yannick.epstein@leanix.net>
2021-04-07 10:16:26 +0200

    INVT-10360 introduce correct breaking change

--- .github/workflows/introduce_breaking_change.py
+++ .github/workflows/introduce_breaking_change.py
@@ -4,4 +4,4 @@ with open(pathlib.Path('./test/index.html'), "w") as file:
     file.write("<h1>Breaking Change</h1>")
 
 with open(pathlib.Path('./test/main.js'), "w") as file:
-    file.write("console.log('Breaking Change')")
+    file.write("console.log('Breaking Change');")
