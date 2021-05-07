0c465c586c83af8ed7ab0d72839bac5b99fa3914
Yannick Epstein <yannick.epstein@leanix.net>
2021-05-07 09:07:07 +0200

    INVT-10721 pass environment variables to sas shell-script

--- .github/workflows/fetch_sas_token.sh
+++ .github/workflows/fetch_sas_token.sh
@@ -2,4 +2,4 @@ az login --service-principal -u ${{ env.ARM_CLIENT_ID }} -p=${{ env.ARM_CLIENT_S
 az account set -s ${{ env.ARM_SUBSCRIPTION_ID }}
 expires="$(TZ=UTC date -d '+2 hours' +%Y-%m-%dT%H:%M:%SZ)"
 sasToken="$(az storage account generate-sas --expiry $expires --permissions r --account-name leanixwesteuropetest --resource-types o --services f --https-only -o json | jq --raw-output)"
-echo "SAS_TOKEN=$(echo $sasToken)" >> $GITHUB_ENV
+echo "::set-env name=SAS_TOKEN::$(echo $sasToken)"
