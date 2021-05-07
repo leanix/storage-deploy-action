36989720a37169ec91bb428a173a4746e508bcf9
Yannick Epstein <yannick.epstein@leanix.net>
2021-05-07 09:15:42 +0200

    INVT-10721 try to fix problems with GitHub environment in shell script

--- .github/workflows/fetch_sas_token.sh
+++ .github/workflows/fetch_sas_token.sh
@@ -1,5 +1,5 @@
-az login --service-principal -u ${{ env.ARM_CLIENT_ID }} -p=${{ env.ARM_CLIENT_SECRET }} --tenant ${{ env.ARM_TENANT_ID }}
-az account set -s ${{ env.ARM_SUBSCRIPTION_ID }}
+az login --service-principal -u $ARM_CLIENT_ID -p=$ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID 
+az account set -s $ARM_SUBSCRIPTION_ID
 expires="$(TZ=UTC date -d '+2 hours' +%Y-%m-%dT%H:%M:%SZ)"
 sasToken="$(az storage account generate-sas --expiry $expires --permissions r --account-name leanixwesteuropetest --resource-types o --services f --https-only -o json | jq --raw-output)"
-echo "::set-env name=SAS_TOKEN::$(echo $sasToken)"
+echo "SAS_TOKEN=$(echo $sasToken)" >> $GITHUB_ENV
